5d9272ba16c27e6e78aeb9d51ae36040d703c9db
karguzhali <karguzhali04.3@gmailcom>
2019-12-17 19:21:14 +0530

    did

--- src/git/tool.java
+++ src/git/tool.java
@@ -1,5 +1,17 @@
 package git;
 
-public class tool {
+public class tool { 
+	int a=234;
+	int b=234;
+	int c=3456789;
+	int a1=234;
+	int b1=234;
+	int c1=3456789;
+	int a2=234;
+	int b2=234;
+	int c2=3456789;
+	
+	
+	
 
 }
